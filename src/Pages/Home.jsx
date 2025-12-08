968f42acd765d98e14c14f532eea12f64f6bdbd3
Gopi <115354419+GopiKyada@users.noreply.github.com>
2025-12-08 15:30:26 +0530

    Update Lottie animation source URL

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -159,7 +159,7 @@ const Home = () => {
 
   // Lottie configuration
   const lottieOptions = {
-    src: "https://lottie.host/58753882-bb6a-49f5-a2c0-950eda1e135a/NLbpVqGegK.lottie",
+    src: "https://lottie.host/2e4ae6d8-30b9-400c-a5d7-798906853273/eZzWXD2z50.lottie",
     loop: true,
     autoplay: true,
     rendererSettings: {
